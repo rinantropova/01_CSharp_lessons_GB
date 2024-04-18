5356f2da355e4fc6d16f6dc24a9d46e54981d7d5
Rina Antropova <rin.antropova@gmail.com>
2024-04-18 22:13:30 +0300

    Task3 is solved, Seminar1

---
+++ Seminar1/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное целое число и на выходе показывает сумму
+// первой и последней цифры этого числа.
+// 456 => 10
+// 782 => 9
+// 918 => 17
+
+Console.Write("Enter 3-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 100 && num <= 999)
+{
+int firstDigit = num / 100; // 456 / 100 = 4,56
+int lastDigit = num %  10; // 456 % 10 = 6
+
+int sum = firstDigit + lastDigit;
+Console.WriteLine($"Sum of the first and last digits of {num}:  {sum}");
+}
+else
+{
+    Console.Write("Incorrect number");
+}
