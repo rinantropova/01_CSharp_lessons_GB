d9e9ebb104a5265647a07edf428f014ca75a6de3
Rina Antropova <rin.antropova@gmail.com>
2024-04-18 21:52:30 +0300

    Update in the Task2

--- Seminar1/Task2/Program.cs
+++ Seminar1/Task2/Program.cs
@@ -8,12 +8,19 @@
 Console.Write("Enter new positive number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int count = -num; // num * -1
-
-while (count <= num)
+if (num > 0)
 {
-    Console.Write(count + " ");
-    count++;
-    // count = count + 1; // count++
-    // count += 1; in Python
+    int count = -num; // num * -1
+
+    while (count <= num)
+    {
+        Console.Write(count + " ");
+        count++;
+        // count = count + 1; // count++
+        // count += 1; in Python
+    }
+}
+else
+{
+    Console.WriteLine("Input error!");
 }
