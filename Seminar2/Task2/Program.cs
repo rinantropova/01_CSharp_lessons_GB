682dc13080bc79f97f1a84c0b8fd12583f75cdce
Rina Antropova <rin.antropova@gmail.com>
2024-04-22 20:52:13 +0300

    Task2 from Sem2 - solution from the seminar

--- Seminar2/Task2/Program.cs
+++ Seminar2/Task2/Program.cs
@@ -1,8 +1,23 @@
 ﻿Console.Write("Enter new 3-digits positive number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
+if (num >= 100 && num <= 999)
+{
 int thirdDigit = num % 10;
-int secondDigit = (num /10) % 10;
-double raised = Math.Pow(secondDigit, thirdDigit);
+int secondDigit = num /10 % 10;
+// double raised = Math.Pow(secondDigit, thirdDigit);
 
-Console.WriteLine($"The result is: {raised}");
+int result = secondDigit;
+int count = 1;
+
+while (count < thirdDigit)
+{
+    result = result * secondDigit; // result *= secondDigit;
+    count++;
+}
+Console.WriteLine($"The result is: {result}");
+}
+else
+{
+    Console.WriteLine("Incorrect input!");
+}
