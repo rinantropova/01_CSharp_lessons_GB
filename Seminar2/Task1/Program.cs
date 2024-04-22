e01f10641c5a4989bb310bce05d0d388ce5845bb
Rina Antropova <rin.antropova@gmail.com>
2024-04-22 20:28:07 +0300

    Task1 from sem2 - teacher's solution

--- Seminar2/Task1/Program.cs
+++ Seminar2/Task1/Program.cs
@@ -1,8 +1,17 @@
-﻿Console.Write("Enter new 3-digits cd positive number: ");
+﻿Console.Write("Enter new 3-digits positive number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
+if (num >= 100 && num <= 999)
+{
 int firstDigit = num /100;
 int lastDigit = num % 10;
 
-Console.WriteLine($"The number is: {firstDigit}{lastDigit}");
+//Console.WriteLine($"The number is: {firstDigit}{lastDigit}");
+int result = firstDigit * 10 + lastDigit;
 
+Console.WriteLine($"The result is {result}");
+}
+else
+{
+    Console.WriteLine("Incorrect input!");
+}
