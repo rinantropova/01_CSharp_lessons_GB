67ba5c52be74e6281fd8440779d5897e53d3d156
Rina Antropova <rin.antropova@gmail.com>
2024-04-22 12:49:58 +0300

    Task4 from seminar2 - attempt to solve it on my own

---
+++ Seminar2/Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter a new positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int thirdDigit = num / 100;
+
+if (thirdDigit >= 10)
+{
+    int finalThird = thirdDigit % 10;
+    Console.WriteLine(finalThird); 
+}
+if (thirdDigit < 10 && thirdDigit > 0)
+{
+    Console.WriteLine(thirdDigit);
+}
+else
+{
+    Console.WriteLine("There is no third digit in your number!");
+}
