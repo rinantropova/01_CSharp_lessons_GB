6f27e69697b854520a389b6eac1b1583cb183a48
Rina Antropova <rin.antropova@gmail.com>
2024-04-22 21:16:46 +0300

    Task3 sem2 - seminar's soluttion

--- Seminar2/Task3/Program.cs
+++ Seminar2/Task3/Program.cs
@@ -1,15 +1,16 @@
-﻿Console.Write("Enter the first number: ");
+﻿Console.Write("Type the first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Enter the second number: ");
+Console.Write("Type the second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 % num2 == 0)
+int remainder = num1 % num2;
+
+if (remainder == 0)
 {
     Console.WriteLine("Yes");
 }
 else
 {
-    int remainder = num1 % num2;
     Console.WriteLine($"No, the remainder is: {remainder}"); 
 }
