16f120e0fc2aa3193936834f00490d6fe4b87418
Rina Antropova <rin.antropova@gmail.com>
2024-04-22 11:50:54 +0300

    Task2 from seminar 2 - self attempt to solve

---
+++ Seminar2/Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter new 3-digits positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int thirdDigit = num % 10;
+int secondDigit = (num /10) % 10;
+double raised = Math.Pow(secondDigit, thirdDigit);
+
+Console.WriteLine($"The result is: {raised}");
