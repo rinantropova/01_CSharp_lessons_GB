7718d4306d3426754fe45de6703542e9e5da4888
Rina Antropova <rin.antropova@gmail.com>
2024-04-22 21:38:25 +0300

    Task4 from seminar - thrid digit from the left

---
+++ Seminar2/Task4-1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Third from the left
+
+Console.Write("Type a new positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 || num <= -100)
+{
+    while (num > 999 || num < -999)
+    {
+        num = num / 10;
+    }
+    int lastDigit = num % 10;
+    Console.WriteLine($"The result is: {lastDigit}");
+}
+else
+{
+    Console.WriteLine("There is no third digit in your number!");
+}
