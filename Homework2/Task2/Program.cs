0c28de11174a3b73d41e6a8cbea6ce09cc7eb45d
Rina Antropova <rin.antropova@gmail.com>
2024-04-23 09:32:07 +0300

    Homework Task2 - coordinates

---
+++ Homework2/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Type the first coordinate: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Type the second coordinate: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+//Console.Write("Type coordinates X and Y with a space: ");
+// string[] coordinates = Console.ReadLine().Split(' ');
+// int x = Convert.ToInt32(coordinates[0]);
+// int y = Convert.ToInt31(coordinates[1]);
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("2");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("3");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("4");
+}
+else
+{
+    Console.WriteLine("Incorrect coordinates!");
+}
