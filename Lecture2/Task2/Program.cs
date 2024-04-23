a09294d9743a856ff2c12d3b9148b59d5b5e6ad6
Rina Antropova <rin.antropova@gmail.com>
2024-04-23 14:25:33 +0300

    Task2 - Lect2

---
+++ Lecture2/Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 10;
+int[] array = new int[n];
+int i = 0;
+
+while (i <array.Length)
+{
+    array[i] = i + 1;
+    // Console.Write(array[i] + ' ');
+    Console.Write($"{array[i]} ");
+    i++;
+}
