4f15fb5ff8f48a66a4ad209e9ffc4a9ffd89fa10
Rina Antropova <rin.antropova@gmail.com>
2024-04-23 10:25:46 +0300

    Homework Task4 - my solution and GB - digits of number with comma

---
+++ Homework2/Task4/Program.cs
@@ -0,0 +1,37 @@
+﻿// Solution from GB
+// Console.Write("Type a new positive number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num < 10)
+// {
+//     Console.WriteLine(num);
+// }
+// else
+// {
+//     while (num > 0)
+//     {
+//         int digit = num % 10;
+//         num /= 10;
+//         //num = num / 10;
+        
+//         if (num > 0)
+//         {
+//             Console.Write(digit + ", ");
+//         }
+//         else
+//         {
+//             Console.WriteLine(digit);
+//         }
+//     }
+// }
+
+// My solution
+Console.Write("Type a new positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+while (num > 0)
+{
+    int digit = num % 10;
+    Console.Write(digit + ", ");
+    num /= 10;
+}
