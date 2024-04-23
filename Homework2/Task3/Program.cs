1d1eabf9f32dac3ace2a3d833a99be6506df5127
Rina Antropova <rin.antropova@gmail.com>
2024-04-23 10:00:41 +0300

    Homework task3 - maxDigit in number

---
+++ Homework2/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Type a new number from array [10, 99]: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 10 && num <= 99)
+{
+    int firstDigit = num % 10;
+    int secondDigit = num /10;
+
+    // int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+    // Console.WriteLine(maxDigit);
+
+    if (firstDigit > secondDigit)
+    {
+        Console.WriteLine($"The biggest digit is: {firstDigit}");
+    }
+    else
+    {
+        Console.WriteLine($"The biggest digit is: {secondDigit}");
+    }
+}
+else
+{
+    Console.WriteLine("Incorrect input!");
+}
