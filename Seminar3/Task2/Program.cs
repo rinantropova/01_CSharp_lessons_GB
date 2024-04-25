455a98b2d37865965eca7d80c997ec1a07cd0ab9
Rina Antropova <rin.antropova@gmail.com>
2024-04-25 21:30:06 +0300

    Sem3 Task1 - updated with the comments from seminar

--- Seminar3/Task2/Program.cs
+++ Seminar3/Task2/Program.cs
@@ -6,11 +6,15 @@
 // => [-1 5 -6]
 
 
-int[] array = {-10, 9, -8, 7, -6, 5, -4, 3, -2, 1};
+int[] array = new int[10] { -10, 9, -8, 7, -6, 5, -4, 3, -2, 1 };
 
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = array[i] * (-1);
-    Console.Write($"{array[i]} ");
+    array[i] = array[i] * (-1); // array[i] *= -1;
+    // Console.Write($"{array[i]} ");
 }
 
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
