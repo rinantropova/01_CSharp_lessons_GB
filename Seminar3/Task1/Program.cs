0ec00c5e41deb38d341eb6205b69e7bd0e178cdb
Rina Antropova <rin.antropova@gmail.com>
2024-04-25 21:02:54 +0300

    Sem3 Task1 - seminar's solution added

--- Seminar3/Task1/Program.cs
+++ Seminar3/Task1/Program.cs
@@ -5,28 +5,54 @@
 // [1 3 4 19 3], 8 => Нет
 // [-4 3 4 1], 3 => Да
 
+// My solution:
+// Console.Write("Type a new number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// // int num = 10;
+// int[] array = { -4, 3, 4, 1 };
+
+// // Console.Write(array.Length);
+// bool found = false;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (num == array[i])
+//     {
+//         found = true;
+//         break;
+//     }
+// }
+// if (found)
+// {
+//     Console.WriteLine($"Yes, number {num} is in the array");
+// }
+// else
+// {
+//     Console.WriteLine($"No, number {num} is not in the array");
+// }
+
+
+
+
+
+// Seminar's solution:
 Console.Write("Type a new number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 // int num = 10;
 int[] array = { -4, 3, 4, 1 };
 
-// Console.Write(array.Length);
-bool found = false;
+bool isExistNum = false;
 
 for (int i = 0; i < array.Length; i++)
 {
-    if (num == array[i])
+    if(array[i] == num)
     {
-        found = true;
+        isExistNum = true;
         break;
     }
 }
-if (found)
-{
-    Console.WriteLine($"Yes, number {num} is in the array");
-}
-else
-{
-    Console.WriteLine($"No, number {num} is not in the array");
-}
+
+// string result = isExistNum ? "YEs" : "No";
+Console.WriteLine(isExistNum ? "YEs" : "No");
