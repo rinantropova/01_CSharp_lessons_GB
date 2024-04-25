a1667487d7b1495e737d082d57602de0e5d07750
Rina Antropova <rin.antropova@gmail.com>
2024-04-25 16:19:53 +0300

    Sem3 - Task3 - my solution before the seminar

--- Seminar3/Task3/Program.cs
+++ Seminar3/Task3/Program.cs
@@ -6,9 +6,29 @@
 // [2 3 1 7 5 6 3] => [6 18 5] (элемент 7 не имеет пары)
 
 
-int[] array = {2, 3, 5, 56, 32, 11, 6, 7};
+int[] array = {2, 3, 5, 56, 32, 11, 6, 4, 12};
 int size = array.Length / 2;
 int[] newArr = new int[size];
 
 // Console.Write(newArr.Length);
 
+int i = 0;
+int j = array.Length - 1;
+
+for (int e = 0; e < newArr.Length; e++)
+{
+    newArr[e] = array[i] * array[j];
+    i++;
+    j--;
+}
+   
+// newArr[1] = array[i] * array[j];
+// i++;
+// j--;
+// newArr[2] = array[i] * array[j];
+// i++;
+// j--;
+// newArr[3] = array[i] * array[j];
+
+// Array.ForEach(newArr, Console.WriteLine);
+Console.WriteLine($"[{string.Join(", ", newArr)}]");
