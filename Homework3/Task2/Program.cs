cf1342062abd5b258c0318953d40dc491689c0e8
Rina Antropova <rin.antropova@gmail.com>
2024-04-26 11:30:21 +0300

    HW3 Task2 solved

---
+++ Homework3/Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = new int[10] {22, 45, 12, 5, 6, 120, 55, 6, 78, 23};
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
