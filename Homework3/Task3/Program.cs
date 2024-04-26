9ccbc1d647c59db164620b58c48f614b1de29391
Rina Antropova <rin.antropova@gmail.com>
2024-04-26 11:35:01 +0300

    HW3 Task3 solved

---
+++ Homework3/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿double[] array = {1.22, 4.5, 3.33};
+double max = array[0];
+double min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    else if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+double result = max - min;
+Console.WriteLine(result);
