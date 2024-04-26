0e2bd9181d4e3044a7dc89fa15b183263f89596a
Rina Antropova <rin.antropova@gmail.com>
2024-04-26 11:16:17 +0300

    HW Task1 done

---
+++ Homework3/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = new int[10] {22, 45, 12, 5, 6, 123, 55, 6, 73, 23};
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] >= 20 && array[i] <= 90)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
