c9e00b751270dc368f60167299bf52d649a53944
Rina Antropova <rin.antropova@gmail.com>
2024-04-29 11:15:35 +0300

    Lect3 task4 - generating random values

---
+++ Lecture3/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// generation of random values
+
+Random rnd = new Random();
+
+int size = 1000;
+int[] arr_int = new int[size];
+int i = 0;
+while(i < size)
+{
+    arr_int[i] = rnd.Next(1, 10);
+    i++;
+}
+
+i = 0;
+while(i < size)
+{
+    Console.Write($"{arr_int[i]} ");
+    i++;
+}
