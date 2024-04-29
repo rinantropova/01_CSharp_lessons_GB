fb3d29e8506c0911140bbaa9584a01912f4e26c6
Rina Antropova <rin.antropova@gmail.com>
2024-04-29 10:56:42 +0300

    Lect3 Task3 added - array&functions

---
+++ Lecture3/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Array in function, changing each even element to 0
+
+void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 2, 4, 6, 7, 8, 8, 9, 10, 11 };
+ZeroEvenElements(array);
+PrintArray(array);
