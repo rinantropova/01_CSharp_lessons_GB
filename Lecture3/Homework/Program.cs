ce37608aec04d2adff3d1c6fcaeb3bfecf66d2d7
Rina Antropova <rin.antropova@gmail.com>
2024-04-29 12:58:25 +0300

    Lect3 - Homework

---
+++ Lecture3/Homework/Program.cs
@@ -0,0 +1,47 @@
+﻿
+void FillArray(int[] array)
+{
+   
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = i + 1;
+    }
+}
+
+void PrintArray(int[] array)
+{
+        for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} " );
+    }
+}
+
+int GetSumOfElements(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int GetProductOfElements(int[] array)
+{
+    int product = 1;
+    for(int i = 0; i < array.Length; i++)
+    {
+        product *= array[i];
+    }
+    return product;
+}
+
+int n = 10;
+int[] arr = new int[n];
+FillArray(arr);
+PrintArray(arr);
+int sum = GetSumOfElements(arr);
+int product = GetProductOfElements(arr);
+Console.Write(Environment.NewLine);
+Console.WriteLine(sum);
+Console.WriteLine(product);
