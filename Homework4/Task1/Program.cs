7c76887ebc65fb46b2bc3278dd855b9bf1fd25a6
Rina Antropova <rin.antropova@gmail.com>
2024-05-14 14:23:48 +0300

    HW4 - Task1 - endless numbers request

--- Homework4/Task1/Program.cs
+++ Homework4/Task1/Program.cs
@@ -4,3 +4,67 @@
 // числа, сумма цифр которого четная.
 // 5 12 16 q [STOP]
 // 3 45 342 15 [STOP]
+
+
+
+// using Internal;
+
+// void endlessInput()
+// {
+//    List<int> addedNumbers = new List<int>();
+//    while(true)
+//     {
+//         Console.WriteLine("Input number ");
+//         int n = Convert.ToInt32(Console.ReadLine());
+//     }
+//     addedNumbers.Add(n);
+//     Console.Write($"{addedNumbers} ");
+// }
+
+
+
+void endlessList()
+{
+    while(true)
+    {
+        Console.WriteLine("Input number or 'q' to stop the program: ");
+        string input = Console.ReadLine();
+        if (input.ToLower() == "q")
+        {
+            Console.Write("[STOP]");
+        }
+        if (int.TryParse(input, out int number))
+        {
+            if (digitSumEven(number))
+            {
+                Console.Write("[STOP]");
+                break;
+            }
+        }     
+    }
+}
+
+bool digitSumEven(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum % 2 == 0;
+}
+
+// bool qSymbolStop(int n)
+// {
+//     if (n == 113)
+//     {
+//         Console.Write("q ");
+//         Console.Write("[STOP]");
+//         return true;
+//     }
+//     return false;
+// }
+
+
+endlessList();
