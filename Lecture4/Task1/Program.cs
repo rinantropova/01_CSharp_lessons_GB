5932466ccb8ab6ccd22dbe63cd81ea333008faf5
Rina Antropova <rin.antropova@gmail.com>
2024-05-15 15:19:09 +0300

    Lecture4 Task1 - solution with functions is added

--- Lecture4/Task1/Program.cs
+++ Lecture4/Task1/Program.cs
@@ -1,27 +1,61 @@
 ﻿
+// INITIAL SOLUTION WITHOUT FUNCTIONS:
+// int[,] table = new int[3, 5];
+// // int[,] - type
+// // table - variable's name
+// // new int[3, 5]; - memory slot selection for the 2D-array, size of the array, 3 - rows, 5 - amount of element in each line
 
-int[,] table = new int[3, 5];
-// int[,] - type
-// table - variable's name
-// new int[3, 5]; - memory slot selection for the 2D-array, size of the array, 3 - lines, 5 - amount of element in each line
+// Random rnd = new Random();
+// for (int i = 0; i < table.GetLength(0); i++)
+// // table.GetLength(0) - request the length - amount of rows or columns, when it's 0 in () - we request amount of rows of 2D array
+// {
+//     for (int j = 0; j < table.GetLength(1); j++)
+//     // table.GetLnegth(1) - we're requesting amount of columns in the array (because of 1)
+//     {
+//         table[i, j] = rnd.Next(1, 11);
+//     }
+// }
 
-Random rnd = new Random();
-for (int i = 0; i < table.GetLength(0); i++)
-// table.GetLength(0) - request the length - amount of lines or columns, when it's 0 in () - we request amount of lines of 2D array
+// for (int i = 0; i < table.GetLength(0); i++)
+// {
+//     for (int j = 0; j < table.GetLength(1); j++)
+//     {
+//         Console.Write($"{table[i, j]} ");
+//         // this method of output is called intropolation
+//     }
+//     Console.WriteLine();
+// }
+
+
+
+//SOLUTION WITH FUNCTIONS:
+int[,] CreateMatrix(int rowCount, int columnsCount)
 {
-    for (int j = 0; j < table.GetLength(1); j++)
-    // table.GetLnegth(1) - we're requesting amount of columns in the array (because of 1)
+    int[,] table = new int[rowCount, columnsCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < table.GetLength(0); i++)
     {
-        table[i, j] = rnd.Next(1, 11);
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return table;
+}
+
+void Showmatrix(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write($"{table[i, j]} ");
+            
+        }
+        Console.WriteLine();
     }
 }
 
-for (int i = 0; i < table.GetLength(0); i++)
-{
-    for (int j = 0; j < table.GetLength(1); j++)
-    {
-        Console.Write($"{table[i, j]} ");
-        // this method of output is called intropolation
-    }
-    Console.WriteLine();
-}
+int[,] matrix = CreateMatrix(4, 5);
+Showmatrix(matrix);
