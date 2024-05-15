1bdfdc2d05bc0d918d8261a25f2b1d9dabfffa41
Rina Antropova <rin.antropova@gmail.com>
2024-05-15 15:56:39 +0300

    Lecture4 Task3 - extracting letters from a string

---
+++ Lecture4/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Create a word from letters in the string
+
+
+string GetLettersFromString(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+Console.WriteLine(result);
