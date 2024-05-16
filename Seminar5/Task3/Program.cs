d4c44e1d92725216dda61b8f603e51687c291199
Rina Antropova <rin.antropova@gmail.com>
2024-05-16 14:31:38 +0300

    Sem5 Task3 - my solution - rowsMean as a new array

---
+++ Seminar5/Task3/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте двумерный массив из целых чисел. Сформируйте новый
+// одномерный массив, состоящий из средних арифметических
+// // значений по строкам двумерного массива. 
+
+
+
+
+int[,] CreateMatrix(int rowCount, int columnsCount)
+{
+    int[,] table = new int[rowCount, columnsCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return table;
+}
+
+void Showmatrix(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write($"{table[i, j]} ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] CalculateRowsMean(int[,] table)
+{
+    int rowCount = table.GetLength(0);
+    int columnCount = table.GetLength(1);
+    double[] means = new double[rowCount];
+
+    for (int i = 0; i < rowCount; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < columnCount; j++)
+        {
+            sum += table[i, j];
+        }
+        means[i] = (double)sum / columnCount;
+    }
+    return means;
+}
+
+int[,] matrix = CreateMatrix(3, 4);
+Showmatrix(matrix);
+double[] rowMeans = CalculateRowsMean(matrix);
+
+foreach (double avg in rowMeans)
+{
+    Console.Write($"{avg:F2} ");
+}
+
+
+
