575390e4540dd9e29c3708b8374396c4fea506a1
Rina Antropova <rin.antropova@gmail.com>
2024-05-17 11:03:59 +0300

    Sem5 - Task1 - updated with the teacher's solution (function is added)

--- Seminar5/Task1/Program.cs
+++ Seminar5/Task1/Program.cs
@@ -1,5 +1,7 @@
-﻿
-int[,] CreateMatrix(int rowCount, int columnsCount)
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба
+// индекса чётные, и замените эти элементы на их квадраты.
+
+int[,] CreateMatrix(int rowCount, int columnsCount, int min, int max)
 {
     int[,] table = new int[rowCount, columnsCount];
 
@@ -8,7 +10,7 @@ int[,] CreateMatrix(int rowCount, int columnsCount)
     {
         for (int j = 0; j < table.GetLength(1); j++)
         {
-            table[i, j] = rnd.Next(1, 11);
+            table[i, j] = rnd.Next(min, max);
         }
     }
     return table;
@@ -18,27 +20,45 @@ void Showmatrix(int[,] table)
 {
     for (int i = 0; i < table.GetLength(0); i++)
     {
+        // Console.Write("|");
         for (int j = 0; j < table.GetLength(1); j++)
         {
-            Console.Write($"{table[i, j]} ");
-            
+            Console.Write($"{table[i, j],4} ");
+
         }
+        // Console.WriteLine(" |");
         Console.WriteLine();
     }
 }
 
-int[,] matrix = CreateMatrix(4, 5);
-Showmatrix(matrix);
-
-for (int i = 0; i < matrix.GetLength(0); i++)
+void EvenIndexElementToSquare(int[,] table)
 {
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    for (int i = 0; i < table.GetLength(0); i += 2)
     {
-        if(i % 2 == 0 && j % 2 == 0)
+        for (int j = 0; j < table.GetLength(1); j += 2)
         {
-            matrix[i, j] *= matrix[i, j];
+            // if(i % 2 == 0 && j % 2 == 0)
+
+            table[i, j] *= table[i, j];
+
         }
     }
 }
 
+int[,] matrix = CreateMatrix(4, 5, 1, 11);
 Showmatrix(matrix);
+EvenIndexElementToSquare(matrix);
+Console.WriteLine();
+Showmatrix(matrix);
+
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         if(i % 2 == 0 && j % 2 == 0)
+//         {
+//             matrix[i, j] *= matrix[i, j];
+//         }
+//     }
+// }
+
