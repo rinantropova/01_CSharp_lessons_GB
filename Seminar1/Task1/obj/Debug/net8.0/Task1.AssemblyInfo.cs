1285d056c7aa1cd8f8f08e9a39946a80bc11d3e3
Rina Antropova <rin.antropova@gmail.com>
2024-05-20 11:33:31 +0300

    VSC random

--- Seminar1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Seminar1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb5f4fde4d250eed8a396560c95cac9386cb132")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+389c1c016c8d0b37d33515901d1157bbed5f6cc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
