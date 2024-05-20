da1629f5509b53f28aa08e5096be7a299eb3147e
Rina Antropova <rin.antropova@gmail.com>
2024-05-20 16:04:25 +0300

    Sem6 - Task3 - vowels in the string - my approach

---
+++ Seminar6/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Считать строку с консоли, состоящую из латинских
+// букв в нижнем регистре. Выяснить, сколько среди
+// введённых букв гласных.
+// Пример
+// “hello” => 2
+// “world” => 1
+
+// My approach:
+Console.WriteLine("Please, enter your sentence: ");
+string str = Console.ReadLine();
+int vowelsInString = GetVowelsFromString(str);
+Console.Write($"Number of vowels is: {vowelsInString}");
+
+int GetVowelsFromString(string letters)
+{
+    var vowels = new HashSet<char> {'a', 'e', 'i', 'o', 'u'};
+
+    int vowelCount = letters.Count(c => vowels.Contains(char.ToLower(c)));
+    int consonantCount = letters.Count(c => char.IsLetter(c) && !vowels.Contains(char.ToLower(c)));
+    return vowelCount;
+
+}
