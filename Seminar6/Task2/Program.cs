c7c359cc4f14259a57cc4d851febe66f7f559c5d
Rina Antropova <rin.antropova@gmail.com>
2024-05-20 15:45:17 +0300

    Sem6 - Task2 - my approach - string to array

---
+++ Seminar6/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// На основе символов строки (тип string) сформировать массив
+// символов (тип char[]). Вывести массив на экран.
+// Указание
+// Метод строки ToCharArray() не использовать.
+
+// Пример
+// “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ] 
+
+string str = "Hello!";
+
+// forbidden way to solve the task:
+// char[] characters = str.ToCharArray();
+// foreach (char letter in characters)
+// {
+//     Console.Write("'" + letter + "" + "'" + ",");
+// }
+
+// My attempt to solve it:
+char[] charArray = new char[str.Length];
+
+for (int i = 0; i < str.Length; i++)
+{
+    charArray[i] = str[i];
+}
+
+Console.Write("[");
+foreach(char letter in charArray)
+{
+    Console.Write("'" + letter + "'" + ", ");
+}
+Console.Write("]");
