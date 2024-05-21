cfb95e2ab0306316f82882727fd62225b9b7e1e5
Rina Antropova <rin.antropova@gmail.com>
2024-05-21 10:55:11 +0300

    HW6 Task4 - Solved by teacher in the sem6 - reverse sentence

---
+++ Homework6/Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую
+// из слов, разделенных пробелами. Сформировать строку,
+// в которой слова расположены в обратном порядке. В
+// полученной строке слова должны быть также разделены
+// пробелами.
+
+// “Hello my world” => “world my Hello”
+
+
+
+static string ReverseWords(string strNew)
+{
+    string[] strNewSplit = strNew.Split();
+
+    string result = "";
+
+    for (int i = strNewSplit.Length - 1; i >= 0; i--)
+    {
+        result += strNewSplit[i] + " ";
+    }
+
+    return result;
+}
+
+
+string strNew = "Hello my world";
+
+string result = ReverseWords(strNew);
+
+Console.WriteLine(result);
