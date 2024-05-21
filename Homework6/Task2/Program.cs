fffc09501068301fecfbbf2259def49b0cf8031a
Rina Antropova <rin.antropova@gmail.com>
2024-05-21 10:48:33 +0300

    HW6 - Task2 - Solved by Teacher in the Sem6 - upper case to lower case

---
+++ Homework6/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы
+// в обоих регистрах. Сформируйте строку, в которой все
+// заглавные буквы заменены на строчные. 
+// “aBcD1ef!-” => “abcd1ef!-”
+
+
+
+
+static string UpperToLower(string str)
+{
+    string strResult = string.Empty;
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (char.IsUpper(str[i]))
+        {
+            strResult += char.ToLower(str[i]);
+        }
+        else
+        {
+            strResult += str[i];
+        }
+    }
+
+    return strResult;
+}
+
+string str = "aBcD1ef!-";
+
+string strResult = UpperToLower(str);
+
+Console.Write(strResult);
