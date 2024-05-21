257cf1c5d56ed7225c7c1d76d5cffbdac37f27dc
Rina Antropova <rin.antropova@gmail.com>
2024-05-21 10:03:35 +0300

    Sem6 - Task2 - Seminar's solution with functions

--- Seminar6/Task2/Program.cs
+++ Seminar6/Task2/Program.cs
@@ -6,7 +6,7 @@
 // Пример
 // “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ] 
 
-string str = "Hello!";
+// string str = "Hello!";
 
 // forbidden way to solve the task:
 // char[] characters = str.ToCharArray();
@@ -15,17 +15,54 @@ string str = "Hello!";
 //     Console.Write("'" + letter + "" + "'" + ",");
 // }
 
+
 // My attempt to solve it:
-char[] charArray = new char[str.Length];
+// char[] charArray = new char[str.Length];
 
-for (int i = 0; i < str.Length; i++)
+// for (int i = 0; i < str.Length; i++)
+// {
+//     charArray[i] = str[i];
+// }
+
+// Console.Write("[");
+// foreach(char letter in charArray)
+// {
+//     Console.Write("'" + letter + "'" + ", ");
+// }
+// Console.Write("]");
+
+
+
+// Solution from the seminar:
+
+char[] StringToCharArray(string s)
 {
-    charArray[i] = str[i];
+    char[] array = new char[s.Length];
+    for (int i = 0; i < s.Length; i++)
+    {
+        array[i] = s[i];
+    }
+    return array;
 }
 
-Console.Write("[");
-foreach(char letter in charArray)
+void PrintArray(char[] array)
 {
-    Console.Write("'" + letter + "'" + ", ");
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.WriteLine(" ]");
 }
-Console.Write("]");
+
+Console.WriteLine("Please, enter symbols: ");
+string str = Console.ReadLine();
+char[] resArray = StringToCharArray(str);
+PrintArray(resArray);
