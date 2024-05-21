6756bdbafb194e56e92d2689ed12fba4dc8467a1
Rina Antropova <rin.antropova@gmail.com>
2024-05-21 10:17:29 +0300

    Sem6 Task3 - Seminar's solution

--- Seminar6/Task3/Program.cs
+++ Seminar6/Task3/Program.cs
@@ -6,17 +6,44 @@
 // “world” => 1
 
 // My approach:
+// Console.WriteLine("Please, enter your sentence: ");
+// string str = Console.ReadLine();
+// int vowelsInString = GetVowelsFromString(str);
+// Console.Write($"Number of vowels is: {vowelsInString}");
+
+// int GetVowelsFromString(string letters)
+// {
+//     var vowels = new HashSet<char> {'a', 'e', 'i', 'o', 'u'};
+
+//     int vowelCount = letters.Count(c => vowels.Contains(char.ToLower(c)));
+//     int consonantCount = letters.Count(c => char.IsLetter(c) && !vowels.Contains(char.ToLower(c)));
+//     return vowelCount;
+
+// }
+
+
+// Seminar's solution:
+
+int CountVowelsLetters(string str)
+{
+    string vowels = "aeiouy";
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        for (int j = 0; j < vowels.Length; j++)
+        {
+            if(vowels[j] == str[i])
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
 Console.WriteLine("Please, enter your sentence: ");
 string str = Console.ReadLine();
-int vowelsInString = GetVowelsFromString(str);
-Console.Write($"Number of vowels is: {vowelsInString}");
+int vowelsInWord = CountVowelsLetters(str);
+Console.WriteLine($"In the string {str} there are {vowelsInWord} vowels");
 
-int GetVowelsFromString(string letters)
-{
-    var vowels = new HashSet<char> {'a', 'e', 'i', 'o', 'u'};
 
-    int vowelCount = letters.Count(c => vowels.Contains(char.ToLower(c)));
-    int consonantCount = letters.Count(c => char.IsLetter(c) && !vowels.Contains(char.ToLower(c)));
-    return vowelCount;
-
-}
